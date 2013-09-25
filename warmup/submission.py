a97b9666ff49f05af251bccf4928b2c7d84ec11b
nmasharani <nisham@>
2013-09-25 10:14:58 -0700

    warmup added

---
+++ warmup/submission.py
@@ -0,0 +1,78 @@
+import collections
+
+############################################################
+# Problem 2a
+
+def computeMaxWordLength(text):
+    """
+    Given a string |text|, return the longest word in |text|.  If there are
+    ties, choose the word that comes latest in the alphabet.  You might find
+    max() and list comprehensions handy here.
+    """
+    # BEGIN_YOUR_CODE (around 1 line of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2b
+
+def createExistsFunction(text):
+    """
+    Given a text, return a function f, where f(word) returns whether |word|
+    occurs in |text| or not.  f should run in O(1) time.  You might find it
+    useful to use set().
+    """
+    # BEGIN_YOUR_CODE (around 4 lines of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2c
+
+def manhattanDistance(loc1, loc2):
+    """
+    Return the Manhattan distance between two locations, where locations are
+    pairs (e.g., (3, 5)).
+    """
+    # BEGIN_YOUR_CODE (around 1 line of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2d
+
+def sparseVectorDotProduct(v1, v2):
+    """
+    Given two sparse vectors |v1| and |v2|, each represented as Counters, return
+    their dot product.
+    You might find it useful to use sum() and a list comprehension.
+    """
+    # BEGIN_YOUR_CODE (around 4 lines of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2e
+
+def incrementSparseVector(v1, scale, v2):
+    """
+    Given two sparse vectors |v1| and |v2|, perform v1 += scale * v2.
+    """
+    # BEGIN_YOUR_CODE (around 2 lines of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
+
+############################################################
+# Problem 2f
+
+def computeMostFrequentWord(text):
+    """
+    Splits the string |text| by whitespace and returns two things as a pair: 
+        the set of words that occur the maximum number of times, and
+	their count, i.e.
+	(set of words that occur the most number of times, that maximum number/count)
+    You might find it useful to use collections.Counter().
+    """
+    # BEGIN_YOUR_CODE (around 5 lines of code expected)
+    raise Exception("Not implemented yet")
+    # END_YOUR_CODE
